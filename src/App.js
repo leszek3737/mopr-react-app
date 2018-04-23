832509c750dc37f0146fecd334b21f8ceaeca8d6
Unknown <leszek@lubcon.org>
2018-04-23 11:44:08 +0200

    Add component App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return this.props.renderApp;
+  }
+  
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -5,30 +5,61 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import Step1 from './Components/Step1';
 
-const dpsList = [
-    {
-    id: 0,
-    koszt: 4000,
-    name: "DPS 1",
-},
-{
-    id: 1,
-    koszt: 3900,
-    name: "DPS 2",
-},
-{
-    id: 2,
-    koszt: 3900,
-    name: "DPS 3",
+window.__data__ = {
+    dps: [{
+            id: 0,
+            koszt: 4000,
+            name: "DPS 1",
+        },
+        {
+            id: 1,
+            koszt: 3900,
+            name: "DPS 2",
+        },
+        {
+            id: 2,
+            koszt: 3900,
+            name: "DPS 3",
+        }
+    ],
+    incomeCriterion: {
+        alone: 1902,
+        family: 1542
+    },
+    numberKinshipDegrees: 4, // -1 
+    kinshipDegree: [{
+            id: 0,
+            name: "Małżonek",
+            mustBeUnique: true,
+        },
+        {
+            id: 1,
+            name: "Dziecko",
+            mustBeUnique: false,
+
+        },
+        {
+            id: 2,
+            name: "Wnuk",
+            mustBeUnique: false,
+        },
+        {
+            id: 3,
+            name: "Rodzic",
+            mustBeUnique: false,
+
+        },
+        {
+            id: 4,
+            name: "Dziadki",
+            mustBeUnique: false,
+        },
+    ],
 }
-];
-let button = {
-    begin: "step1",
-    target: "step2",
-}
+
 let renderApp = (
 <section className="box">
-    <Step1 dpsList={dpsList} button={button} legend="Wybierz dom pomocy społecznej"/>
+    {/* <Step1 dpsList={dpsList} button={button} legend="Wybierz dom pomocy społecznej"/> */}
 </section>
 )
 
