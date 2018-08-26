9ed6e980c9221e91368e4a354cceae51bbe121c5
Unknown <leszek@lubcon.org>
2018-08-27 00:58:18 +0200

    [WIP] Add Step4

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react'
 import Start from "./Step/Start"
 import Step2 from "./Step/Step2"
 import Step3 from "./Step/Step3"
+import Step4 from "./Step/Step4"
 
 export default class App extends Component {
   constructor() {
       super()
       this.state = {
         value: 0,
-        date:{
+        data:{
             dps : [
                 {
                   id: 0,
@@ -24,10 +25,34 @@ export default class App extends Component {
                   name: "DPS 3"
                 }
               ],
+              kinshipDegree : [
+                {
+                  id: 0,
+                  name: "Małżonek",
+                  mustBeUnique: true
+                }, {
+                  id: 1,
+                  name: "Dziecko",
+                  mustBeUnique: false
+                }, {
+                  id: 2,
+                  name: "Wnuk",
+                  mustBeUnique: false
+                }, {
+                  id: 3,
+                  name: "Rodzic",
+                  mustBeUnique: false
+                }, {
+                  id: 4,
+                  name: "Dziadki",
+                  mustBeUnique: false
+                }
+              ],
         },
         dps:0,
         inhabitant:0,
         hasfamily: false,
+        family:[],
     }
   }
     changeState = (data)=>{
@@ -36,13 +61,16 @@ export default class App extends Component {
   render() {
     const content = [
         ()=>{
-            return <Start dps={this.state.date.dps} changeState={this.changeState}/>   
+            return <Start dps={this.state.data.dps} changeState={this.changeState}/>   
         },
         ()=>{
             return <Step2 changeState={this.changeState}/>
         },
         ()=>{
             return <Step3 changeState={this.changeState}/>
+        },
+        ()=>{
+            return <Step4 kinshipDegree={this.state.data.kinshipDegree} changeState={this.changeState}/>
         },
     ]
     return (
---
+++ src/components/Step/Step4.js
@@ -0,0 +1,96 @@
+import React, {Component} from 'react';
+import Button from '../Utils/Button';
+import Input from '../Utils/Input';
+import SelectOptionsItems from '../Utils/SelectOptionsItems';
+
+export default class Step4 extends Component {
+    constructor(){
+        super();
+        this.state = {
+            value:0, 
+            useSpouse: "false",   
+            kinshipDegree: 1,
+            peopleFamily: 1,
+            familyIncome: 0,
+            families:[],
+        }
+    }
+    addFamily = ()=>{
+        const family = {
+            kinshipDegree: this.state.kinshipDegree,
+            peopleFamily: this.state.peopleFamily,
+            familyIncome: this.state.familyIncome,
+            familyPayment: 0,
+        }
+        this.setState({
+            kinshipDegree:1,
+            peopleFamily: 1,
+            familyIncome: 0,
+            families: [...this.state.families, family]
+        })
+    }
+    handleChange = (event)=>{
+        this.setState({
+            [event.target.name]:event.target.value
+        })
+    }    
+    actionNextStep = () => {
+            this.props.changeState({value:0})
+      }
+    componentWillUnmount(){
+          }
+    render() {
+        const list = ()=> {
+            return this.props.kinshipDegree.slice()
+        }
+        return (
+            <div className="step4">
+        <table className="step4__tabele">
+          <tbody>
+            <tr className="step4__tabele--tr">
+              <th className="step4__tabele--th">Rodzaj pokrewieństwa</th>
+              <th className="step4__tabele--th">Ilość osób w gospodarstwie domowym</th>
+              <th className="step4__tabele--th">łączny przychód gospodarstwa domowego</th>
+              <th></th>
+            </tr>
+             {//this
+            //   .state
+            //   .familyMembers
+            //   .map((x) => {
+            //     return x
+            //   })
+        }
+          </tbody>
+        </table>
+        <form>
+            <p>Pokrewieństwo</p> 
+           <select className="step4__tabele--element" onChange={this.handleChange} name="kinshipDegree" value={this.state.kinshipDegree}>
+                <SelectOptionsItems list={list()}/>
+             </select> <br/>
+             <p>Ilość osób w gospodarstwie domowym</p> <br/>
+             <Input 
+          className="step4__tabele--element"
+          type="number"
+          step="1"
+          name="peopleFamily"
+          value={this.state.peopleFamily}
+            onChange={this.handleChange}/> <br/>
+            <p>łączny przychód gospodarstwa domowego</p> <br/>
+            <Input 
+          className="step4__tabele--element"
+          type="number"
+          step="0.01"
+          name="familyIncome"
+          value={this.state.familyIncome}
+            onChange={this.handleChange}/><br/>
+            <Button type="button" kind="button" href="" className={"addFamyli"} lebel={"dodaj rodzine"} click={this.addFamily}/> <br/>
+        </form>
+        <div className="step4__btn">
+        <Button kind="button" className={"nextStep"} lebel={"dalej"} click={this.actionNextStep}/>
+        </div>
+      </div>
+          );
+    }
+}
+
+
